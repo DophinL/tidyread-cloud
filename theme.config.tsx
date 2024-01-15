5173e98fedac7495291f9940a14ad1d63871cf65
jaredliu <jaredliu233@gmail.com>
2024-01-15 21:02:04 +0800

    init

---
+++ theme.config.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+import Image from "next/image";
+// import { useRouter } from "next/router";
+
+const config: DocsThemeConfig = {
+    logo: (
+        <Image
+            src="/images/tidyread_logo.png"
+            alt=""
+            width={120}
+        ></Image>
+    ),
+    // project: {
+    //     link: "https://github.com/shuding/nextra-docs-template",
+    // },
+    // chat: {
+    //     link: "https://discord.com",
+    // },
+    head: (
+        <>
+            <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1.0"
+            />
+            <meta
+                property="og:description"
+                content="An AI-Powered tool that subscribes to RSS feeds and automatically creates concise digests for efficient information consumption."
+            />
+            <meta
+                property="keywords"
+                content="Tidyread, RSS digest, information summary, AI-powered content, news aggregation, efficient reading, personalized news, Docs, Help Center, Support"
+            />
+            <link rel="icon" href="/favicon.svg" />
+        </>
+    ),
+    docsRepositoryBase: "https://github.com/DophinL/tidyread-cloud/tree/main",
+    footer: {
+        text: "Build with ❤️",
+    },
+    useNextSeoProps() {
+        // const { asPath } = useRouter();
+        // if (asPath !== "/") {
+            return {
+                titleTemplate: "%s – Tidyread",
+            };
+        // }
+    },
+};
+
+export default config;
