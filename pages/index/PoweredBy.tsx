9361dd91e8ad0ac4f50bf94942df7d4981f9f5ca
jaredliu <jaredliu233@gmail.com>
2024-01-18 18:07:54 +0800

    chore: 补充首页i18n

---
+++ pages/index/PoweredBy.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+export default function PoweredBy() {
+    return (
+        <div className="flex flex-wrap gap-8">
+            <Image
+                className="mr-4"
+                src="/images/raycast_logo.png"
+                width={123 * 1.3}
+                height={32 * 1.3}
+                alt=""
+            />
+            <Image
+                src="/images/openai_logo.png"
+                width={130 * 1.3}
+                height={32 * 1.3}
+                alt=""
+            />
+            <Image
+                src="/images/moonshot_logo.png"
+                width={180 * 1.3}
+                height={32 * 1.3}
+                alt=""
+            />
+        </div>
+    );
+}
