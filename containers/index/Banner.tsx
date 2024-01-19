ef6308d5a780f770cc7200874eeda2614bcd1685
jaredliu <jaredliu233@gmail.com>
2024-01-19 17:04:06 +0800

    chore: change homepage style

--- containers/index/Banner.tsx
+++ containers/index/Banner.tsx
@@ -1,19 +1,22 @@
+import Image from "next/image";
 import { genUseTranslation } from "../../lib/translation";
 
 const locales = {
-  'en-US': {
-    'follow': 'Follow along on X(Twitter)',
-    'header': 'Lightweight reading flow powered by Raycast and AI',
-    'description': 'A Raycast Extension, which can schedule your daily reading and get tidy, AI-powered digests for an efficient reading experience',
-    'get-started': 'Get Started',
-  },
-  'zh-CN': {
-    'follow': '在 X(Twitter) 上关注我们的进展',
-    'header': '基于 Raycast 和 AI 的轻量级阅读插件',
-    'description': '一款 Raycast 插件，可以用它安排你的日常阅读时间，并获得整洁的、由 AI 提供的摘要，以获得高效的阅读体验',
-    'get-started': '开始使用',
-  }
-}
+    "en-US": {
+        follow: "Follow along on X(Twitter)",
+        header: "AI-Powered Lightweight Reading Extension\nInstant Access at Your Fingertips",
+        description:
+            "A Raycast Extension designed to manage and schedule your daily reading which offers a modern reading experience through AI-powered digest",
+        "get-started": "Get Started",
+    },
+    "zh-CN": {
+        follow: "在 X(Twitter) 上关注进展",
+        header: "AI驱动的轻量资讯阅读插件\n弹指间即可唤起",
+        description:
+            "一款 Raycast 插件，用它来管理和计划你的日常阅读。\n并通过 AI 摘要、整洁的简报使你获得更现代的阅读体验",
+        "get-started": "开始使用",
+    },
+};
 
 const useTranslation = genUseTranslation(locales);
 
@@ -28,20 +31,20 @@ export default function Banner() {
                     target="_blank"
                     href="https://twitter.com/jaredliu_bravo"
                 >
-                    {t('follow')}
+                    {t("follow")}
                 </a>
-                <h1 className="font-heading text-2xl sm:text-4xl md:text-5xl lg:text-6xl gradient-text">
-                    {t('header')}
+                <h1 className="font-heading text-2xl sm:text-3xl md:text-4xl lg:text-5xl gradient-text whitespace-pre lg:leading-tight">
+                    {t("header")}
                 </h1>
                 <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
-                    {t('description')}
+                    {t("description")}
                 </p>
                 <div className="space-x-4">
                     <a
                         className="inline-flex items-center justify-center text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background bg-primary text-primary-foreground hover:bg-primary/90 h-11 px-8 rounded-md"
                         href="/docs/getting-started"
                     >
-                        {t('get-started')}
+                        {t("get-started")}
                     </a>
                     <a
                         target="_blank"
@@ -52,6 +55,14 @@ export default function Banner() {
                         GitHub
                     </a>
                 </div>
+            </div>
+            <div className="flex justify-center">
+                <Image
+                    src="/images/tidyread_intro.png"
+                    width={750}
+                    height={474}
+                    alt="product_intro"
+                />
             </div>
         </section>
     );
