16a51745fae864daa2393f63806e51e595e83e71
jaredliu <jaredliu233@gmail.com>
2024-01-19 20:21:20 +0800

    chore: add redirect

--- next.config.js
+++ next.config.js
@@ -1,14 +1,23 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+const withNextra = require("nextra")({
+    theme: "nextra-theme-docs",
+    themeConfig: "./theme.config.tsx",
+});
 
 module.exports = withNextra({
-  experimental: {
-    appDir: true,
-  },
-  i18n: {
-    locales: ['en-US', 'zh-CN'],
-    defaultLocale: 'en-US'
-  }
-})
+    experimental: {
+        appDir: true,
+    },
+    i18n: {
+        locales: ["en-US", "zh-CN"],
+        defaultLocale: "en-US",
+    },
+    redirects: async () => {
+        return [
+            {
+                source: "/docs",
+                destination: "/docs/getting-started",
+                permanent: true,
+            },
+        ];
+    },
+});
