17ce9b03c22a6071bbd56fd77938e4ff427af629
jaredliu <jaredliu233@gmail.com>
2024-01-22 16:53:39 +0800

    chore: 添加posthog

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
+import { TrackProvider } from "@/components/TrackProvider";
 import "@/lib/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <TrackProvider
+            config={{
+                api_key: "phc_a9a12fmYqiXgkcM5rq72c49HBr0Kx5LvE9Zuuv9QOaP",
+                api_host: "https://app.posthog.com",
+                autocapture: false,
+            }}
+        >
+            <Component {...pageProps} />
+        </TrackProvider>
+    );
 }
 
-export default MyApp
+export default MyApp;
