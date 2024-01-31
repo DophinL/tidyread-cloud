45ac180aa199016b58b1ea2621ccd5daf30a58fc
jaredliu <jaredliu233@gmail.com>
2024-01-31 17:50:19 +0800

    doc

---
+++ components/BlueButton.tsx
@@ -0,0 +1,14 @@
+export default function BlueButton(props: {
+    children: React.ReactNode;
+    onClick?: () => void;
+}) {
+    const { children, onClick } = props;
+    return (
+        <button
+            className="bg-blue-500 hover:bg-blue-700 text-white py-1 px-3 rounded"
+            onClick={onClick}
+        >
+            {children}
+        </button>
+    );
+}
