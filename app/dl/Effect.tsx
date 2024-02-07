034f274840eb2b6c1c8be52fcc54efbd69e3e53d
jaredliu <jaredliu233@gmail.com>
2024-02-07 22:57:36 +0800

    change to open

--- app/dl/Effect.tsx
+++ app/dl/Effect.tsx
@@ -26,9 +26,11 @@ export default function Effect({ kw }: { kw?: string }) {
         defaultSearchText: decodedKeyword,
       };
       // 这里执行跳转
-      window.location.href = `raycast://extensions/jaredliu233/tidyread---streamline-your-daily-reading/add-source.command?context=${encodeURIComponent(JSON.stringify(context))}`;
+      window.open(
+        `raycast://extensions/jaredliu233/tidyread---streamline-your-daily-reading/add-source.command?context=${encodeURIComponent(JSON.stringify(context))}`,
+      );
       window.close();
-    }, 1000);
+    }, 300);
   }, [kw]);
   return null;
 }
